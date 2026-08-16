a70949dd6570af6f6550dc929e2d6ffdb857ebd0
Kavya <kavyamistry0602@gmail.com>
2026-08-17 01:49:28 +0530

    Add Android JARVIS APK project

---
+++ android/app/src/main/java/com/jarvis/hud/MainActivity.java
@@ -0,0 +1,42 @@
+package com.jarvis.hud;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setMediaPlaybackRequiresUserGesture(false);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.setBackgroundColor(0xFF000000);
+
+        setContentView(webView);
+
+        webView.loadUrl("file:///android_asset/web/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
